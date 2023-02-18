339deb3603d0ab4221e4cf5ea6b4d70789d3633e
Jayoooppp <patel.js@somaiya.edu>
2023-02-18 06:53:27 +0530

    Expense changes

--- frontend/pages/admin/tables.js
+++ frontend/pages/admin/tables.js
@@ -21,11 +21,16 @@ import {
   UncontrolledTooltip,
 } from "reactstrap";
 // layout for this page
+import data from "../../assets/data.json"
 import Admin from "layouts/Admin.js";
 // core components
 import Header from "components/Headers/Header.js";
 
 function Tables() {
+  const cashInflow = data.output_fields.values.cash_inflow;
+  const cashOutflow = data.output_fields.values.cash_outflow;
+  console.log(cashOutflow);
+
   return (
     <>
       <Header />
@@ -33,7 +38,9 @@ function Tables() {
       <Container className="mt--7" fluid>
         {/* Table */}
         <Row>
+
           <div className="col">
+
             <Card className="shadow">
               <CardHeader className="border-0">
                 <h3 className="mb-0">Personal expenses</h3>
@@ -50,157 +57,54 @@ function Tables() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">
-                      {/* <Media className="align-items-center">
-                        <a
-                          className="avatar rounded-circle mr-3"
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <img
-                            alt="..."
-                            src={require("assets/img/theme/bootstrap.jpg")}
-                          />
-                        </a>
-                      </Media> */}
-                        <Media>
-                          <span className="mb-0 text-sm">
-                            Food
-                          </span>
-                        </Media>
-                    </th>
-                    <td>25000</td>
-                    <td>
-                      <Badge color="" className="badge-dot mr-4">
-                        <i className="bg-warning" />
-                        High
-                      </Badge>
-                    </td>
-                    {/* <td>
-                      <div className="avatar-group">
-                        <a
-                          className="avatar avatar-sm"
-                          href="#pablo"
-                          id="tooltip742438047"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <img
-                            alt="..."
-                            className="rounded-circle"
-                            src={require("assets/img/theme/team-1-800x800.jpg")}
-                          />
-                        </a>
-                        <UncontrolledTooltip
-                          delay={0}
-                          target="tooltip742438047"
-                        >
-                          Ryan Tompson
-                        </UncontrolledTooltip>
-                        <a
-                          className="avatar avatar-sm"
-                          href="#pablo"
-                          id="tooltip941738690"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <img
-                            alt="..."
-                            className="rounded-circle"
-                            src={require("assets/img/theme/team-2-800x800.jpg")}
-                          />
-                        </a>
-                        <UncontrolledTooltip
-                          delay={0}
-                          target="tooltip941738690"
-                        >
-                          Romina Hadid
-                        </UncontrolledTooltip>
-                        <a
-                          className="avatar avatar-sm"
-                          href="#pablo"
-                          id="tooltip804044742"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <img
-                            alt="..."
-                            className="rounded-circle"
-                            src={require("assets/img/theme/team-3-800x800.jpg")}
-                          />
-                        </a>
-                        <UncontrolledTooltip
-                          delay={0}
-                          target="tooltip804044742"
-                        >
-                          Alexander Smith
-                        </UncontrolledTooltip>
-                        <a
-                          className="avatar avatar-sm"
-                          href="#pablo"
-                          id="tooltip996637554"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <img
-                            alt="..."
-                            className="rounded-circle"
-                            src={require("assets/img/theme/team-4-800x800.jpg")}
-                          />
-                        </a>
-                        <UncontrolledTooltip
-                          delay={0}
-                          target="tooltip996637554"
-                        >
-                          Jessica Doe
-                        </UncontrolledTooltip>
-                      </div>
-                    </td> */}
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">80%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="80"
-                            barClassName="bg-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                    {/* <td className="text-right">
-                      <UncontrolledDropdown>
-                        <DropdownToggle
-                          className="btn-icon-only text-light"
-                          href="#pablo"
-                          role="button"
-                          size="sm"
-                          color=""
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <i className="fas fa-ellipsis-v" />
-                        </DropdownToggle>
-                        <DropdownMenu className="dropdown-menu-arrow" right>
-                          <DropdownItem
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Action
-                          </DropdownItem>
-                          <DropdownItem
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Another action
-                          </DropdownItem>
-                          <DropdownItem
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Something else here
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </UncontrolledDropdown>
-                    </td> */}
-                  </tr>
-                  
+
+                  {
+                    Object.keys(cashOutflow).map((item) => {
+                      return (
+
+                        <>
+                          <tr>
+                            <th scope="row">
+                              <Media>
+                                <span className="mb-0 text-sm">
+                                  {item}
+                                </span>
+                              </Media>
+                            </th>
+                            <td>{cashOutflow[item].amount}</td>
+                            <td>
+                              {(cashOutflow[item].amount / 100000) * 100 > 40 ? (
+
+                                <Badge color="" className="badge-dot mr-4">
+                                  <i className="bg-warning" />
+                                  High
+                                </Badge>
+                              ) : (
+                                <Badge color="" className="badge-dot mr-4">
+                                  <i className="bg-success" />
+                                  Normal
+                                </Badge>
+
+                              )}
+                            </td>
+                            <td>
+                              <div className="d-flex align-items-center">
+                                <span className="mr-2">{Math.ceil((cashOutflow[item].amount / 100000) * 100)} %</span>
+                                <div>
+                                  <Progress
+                                    max="100"
+                                    value={(cashOutflow[item].amount / 100000) * 100}
+                                    barClassName={(cashOutflow[item].amount / 100000) * 100 > 40 ? "bg-danger" : "bg-success"}
+                                  />
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        </>
+                      )
+                    })
+                  }
+
                 </tbody>
               </Table>
               <CardFooter className="py-4">
